4d8935bd7af74e0554eda7be930186f7c872b993
Alex Dumitrescu <daledum@outlook.com>
2017-01-25 18:15:49 -0100

    Create dialog asking the user if they want the continuous GPS sampling mode

--- app/src/main/java/net/monicet/monicet/MainActivity.java
+++ app/src/main/java/net/monicet/monicet/MainActivity.java
@@ -1,5 +1,7 @@
 package net.monicet.monicet;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,7 +19,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // Step 1 starts here:
+        // using the data from resources (containing specie names, photos and description),
+        // create the first Location, then instantiate a Trip (which will contain one location)
+        // and feed its array of sightings to the custom ListView ArrayAdapter
         String[] species = getResources().getStringArray(R.array.speciesArray);
         // TODO: implement later
         String[] photos = new String[30];
@@ -39,12 +44,40 @@ public class MainActivity extends AppCompatActivity {
 
         Location location = new Location(sightings);
         //this will create a trip with one location
-        Trip trip = new Trip(location);
+        final Trip trip = new Trip(location);
 
-        //next time, do this after the addLocation()
+        //next time, do this after the addLocation():
+        // sightingAdapter.clear(); sightingAdapter.add(trip.getLastLocation().getSightings());
+
         SightingAdapter sightingAdapter = new SightingAdapter(this, trip.getLastLocation().getSightings());
 
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(sightingAdapter);
+        // Step 1 ends here
+
+        // Step 2 starts here:
+        // open a dialog and ask the user if they want the Continuous GPS Tracking Mode
+        // if 'yes', set the trip variable accordingly
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("GPS Tracking");
+        alertDialogBuilder.setMessage("You are currently in the normal GPS sampling mode." +
+                "\nActivate the continuous GPS sampling mode, which uses a lot of battery?");
+        alertDialogBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                trip.setContinuousGpsTrackingOn(true);
+            }
+        });
+        alertDialogBuilder.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // do nothing
+                // dialog.dismiss();
+            }
+        });
+        alertDialogBuilder.create();
+        alertDialogBuilder.show();
+        // Step 2 ends here
+
     }
 }
